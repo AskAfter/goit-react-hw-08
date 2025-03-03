978c48cfe1c9c25091c474eba3f64217ae9f88f3
AskAfter <askafter904@gmail.com>
2025-03-03 13:15:09 +0100

    trying rename

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import PhoneBook from '../Somewrap/Phonebook';
+import PhoneBook from '../Phonebook/Phonebook';
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/phonebook" element={<PhoneBook />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
