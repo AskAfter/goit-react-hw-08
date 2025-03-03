ef2db6cfaeb5054b1a66cad0f5d04639846e2258
AskAfter <askafter904@gmail.com>
2025-03-03 18:49:39 +0100

    add reducer to stor and selectors of auth

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filterReducer } from './filters/slice';
 import { contactFormReducer } from './contacts/slice';
+import { authReducer } from './auth/slice';
 
 export const store = configureStore({
   reducer: {
     contacts: contactFormReducer,
     filter: filterReducer,
+    auth: authReducer,
   },
 });
